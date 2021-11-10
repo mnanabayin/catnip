447fb20708201cc87f28459358a80768a7892d00
unknown <mnanabayin@gmail.com>
2021-11-11 02:54:00 +0300

    fix css 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Category from './components/Category'
 
 function App() {
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,4 +1,5 @@
 import React from "react" 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     Container,Row,Col,Image
   } from 'react-bootstrap'
